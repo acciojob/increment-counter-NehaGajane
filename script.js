62543db6d13fb1cd79bf6da0e1ffc01bcd4dddfd
NehaGajane <137098317+NehaGajane@users.noreply.github.com>
2024-01-31 18:13:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let counter = document.getElementById('counter');
+let btn = document.getElementById('incrementBtn');
+let count = 0;
+
+btn.addEventListener('click', function() {
+	counter.textContent = count+1;
+	count++;
+setTimeout(() => alert(count-1));
+})
